392df1ce4d2eaa77b4b56fd24e44099e5f985fa9
Andrey Belykh <abelykh@yahoo.ca>
2019-08-27 10:41:45 -0400

    Initial

---
+++ Main/settings.h
@@ -0,0 +1,15 @@
+#ifndef __VIDEOSETTINGS_H__
+#define __VIDEOSETTINGS_H__
+
+// Colors
+#define BACK_COLOR 0x10
+#define FORE_COLOR 0x3F
+//#define BACK_COLOR 0x00
+//#define FORE_COLOR 0x0C
+
+// Resolution
+#define TIMING timing_800x600_56hz
+#define	HSIZE_CHARS  36
+#define	VSIZE_CHARS  37
+
+#endif /* __VIDEOSETTINGS_H__ */
